a7a50d2139a9b66634c23316db76ada573b39e5e
Blacky <juancontre91@gmail.com>
2016-03-27 23:33:30 -0400

    Codigo cenit polar

---
+++ cen-pol.py
@@ -0,0 +1,12 @@
+
+if __name__== "__main__":
+    
+    cenit = "cenit"
+    polar = "polar"
+    cad = raw_input("Palabra a encriptar: ")
+    for i in range(len(cad)):
+        if cad[i] in polar:
+            print cenit[polar.find(cad[i])]
+        elif cad[i] in cenit:
+            print polar[cenit.find(cad[i])]
+  
